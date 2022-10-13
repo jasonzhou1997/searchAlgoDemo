7f640b202c3b697e7242cf0bb2d7e69a8549adc6
jasonzhou1997 <60407587+jasonzhou1997@users.noreply.github.com>
2022-10-12 21:21:26 -0400

    Created a branch to develop

--- searchAlgoDemo/globalConstant.h
+++ searchAlgoDemo/globalConstant.h
@@ -17,4 +17,26 @@ const int IGNORE_NUM_CHARACTERS = 50;
 const int EXE_INPUT_ARG_POSITION = 0;
 
 const int TOTAL_INPUT_ARG_COUNT = 1;
+
+//const int CELL_SIZE = 8;
+//
+//const int FONT_HEIGHT = 16;
+//const int FONT_WIDTH = 9;
+//
+//const int SCREEN_RESIZE = 4;
+// 
+//The size of the cell
+constexpr unsigned char CELL_SIZE = 16;
+//Number of columns
+constexpr unsigned char COLUMNS = 8;
+//Hmm, I think this is the duration of the effect. But I'm not sure
+constexpr unsigned char EFFECT_DURATION = 16;
+//Vertical size of the font
+constexpr unsigned char FONT_HEIGHT = 16;
+//Horizontal size of the font
+constexpr unsigned char FONT_WIDTH = 8;
+//Number of rows
+constexpr unsigned char ROWS = 8;
+//Screen resize
+constexpr unsigned char SCREEN_RESIZE = 4;
 #endif // !_GLOBALCONSTANT_H
--- searchAlgoDemo/main.cpp
+++ searchAlgoDemo/main.cpp
@@ -1,4 +1,5 @@
 #include <sfml/Graphics.hpp>
+
 #include <iostream>
 #include "globalConstant.h"
 #include "menuOption.h"
@@ -44,6 +45,7 @@ int main(int argc, char *argv[])
 		}
 		if (userOption == GREEDY_OPTION)
 		{
+
 			std::cout << "Greedy Algorithm runs here" << "\n";
 		}
 		if (userOption == DIJSTRA_OPTION)
@@ -53,6 +55,31 @@ int main(int argc, char *argv[])
 		if (userOption == MAKE_MAP_OPTION)
 		{
 			std::cout << "A new map will be made here" << "\n";
+			std::cout << "Enter the width and height of the map: " << "\n";
+			//const int ROWS = []()-> int {
+			//	int t;
+			//	std::cin >> t;
+			//	return t;
+			//}();
+			//const int COLS = []()-> int {
+			//	int t;
+			//	std::cin >> t;
+			//	return t;
+			//}();
+			//const int ROWS = 8;
+			//const int COLS = 8;
+
+			// SFML window
+			sf::RenderWindow window(sf::VideoMode(CELL_SIZE * COLUMNS * SCREEN_RESIZE, SCREEN_RESIZE * (FONT_HEIGHT + CELL_SIZE * COLUMNS)), "Minesweeper", sf::Style::Close);
+			//Here we're resizing the window
+			window.setView(sf::View(sf::FloatRect(0.0, 0.0, CELL_SIZE * COLUMNS, FONT_HEIGHT + CELL_SIZE * ROWS)));
+
+			while (window.isOpen())
+			{
+
+			}
+
+
 		}
 	} while (userOption != EXIT_PROGRAM_OPTION);
 	std::cout << "Thanks for using this program" << "\n";
